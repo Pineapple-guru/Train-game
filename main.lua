1723b9c076c8700274c867d03bc43b3c92ee45e3
James Ferguson <jamessferguson@student.cofo.edu>
2022-11-05 13:47:30 -0500

    Update main.lua; track limit & win condition
    Added variables and conditional statements for measuring how many tracks the player has left to use. When they run out of tracks, they cannot place more and must remove some if they want to continue. If the destination is reached, text is triggered.

--- main.lua
+++ main.lua
@@ -10,7 +10,9 @@ function love.load()
     mousey = 0
     mouse_floor_x = 0
     mouse_floor_y = 0
-    trainTracks = {}
+    track_limit = 16
+    success = false
+    bg = love.graphics.newImage("background.png")
     object_tracker = {}
     for i = 0,15 do
         table.insert(object_tracker, {})
@@ -23,20 +25,30 @@ function love.load()
     object_tracker[3][6] = start_tile
     local end_tile = Tile(12 * x, 1 * y, "end", "end_tile.png")
     object_tracker[13][2] = end_tile
+    end_position = {12, 1}
     current_tile_stack = {}
     table.insert(current_tile_stack, {2, 5})
+    local mountain_tile = Tile(6 * x, 3 * y, "mountain", "mountain_tile.png")
+    object_tracker[7][4] = mountain_tile
+    local mountain_tile = Tile(1 * x, 8 * y, "mountain", "mountain_tile.png")
+    object_tracker[2][9] = mountain_tile
+    local mountain_tile = Tile(6 * x, 4 * y, "mountain", "mountain_tile.png")
+    object_tracker[7][5] = mountain_tile
 end
 
 function love.update(dt)
     mousex, mousey = love.mouse.getPosition()
     mouse_floor_x = math.floor(mousex / 64)
     mouse_floor_y = math.floor(mousey / 64)
-    if love.mouse.isDown(1) then
+    if (current_tile_stack[table.maxn(current_tile_stack)][1] - 1 == end_position[1] and current_tile_stack[table.maxn(current_tile_stack)][2] == end_position[2]) or (current_tile_stack[table.maxn(current_tile_stack)][1] + 1 == end_position[1] and current_tile_stack[table.maxn(current_tile_stack)][2] == end_position[2]) or (current_tile_stack[table.maxn(current_tile_stack)][1] == end_position[1] and current_tile_stack[table.maxn(current_tile_stack)][2] - 1 == end_position[2]) or (current_tile_stack[table.maxn(current_tile_stack)][1] == end_position[1] and current_tile_stack[table.maxn(current_tile_stack)][2] == end_position[2] + 1) then
+        success = true
+    end
+    if love.mouse.isDown(1) and track_limit > 0 then
         if ((mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] - 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] + 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] - 1) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] + 1)) and check_square(mouse_floor_x, mouse_floor_y) then
             local track = Tile(mouse_floor_x * x, mouse_floor_y * y, "track", "track_tile.png")
-            table.insert(trainTracks, track)
             object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = track
             table.insert(current_tile_stack, {mouse_floor_x, mouse_floor_y})
+            track_limit = track_limit - 1
         end
     end
     if love.mouse.isDown(2) then
@@ -44,20 +56,21 @@ function love.update(dt)
             local empty_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "empty", "empty_tile.png")
             object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = empty_tile
             table.remove(current_tile_stack, table.maxn(current_tile_stack))
+            track_limit = track_limit + 1
         end
     end
 end
 
 function love.draw()
-    for i = 0,15 do
-        for j = 0,8 do
-            love.graphics.rectangle("line", x * i, y * j, gridw, gridh)
-        end
-    end
+    love.graphics.draw(bg, 0, 0)
     for i, column in ipairs(object_tracker) do
         for j, tile in ipairs(column) do
             tile:draw()
         end
+    end
+    love.graphics.print("Number of tracks left: " .. track_limit, 0, 0)
+    if success then
+        love.graphics.print("Congratulations, you beat the level!", 524, 0)
     end
 end
 
