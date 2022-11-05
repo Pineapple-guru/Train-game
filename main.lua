fc4c27fa0b11ba98bb4bfe74d4d4a19941202331
James Ferguson <jamessferguson@student.cofo.edu>
2022-11-04 23:10:30 -0500

    Update main.lua; remove tile creation bug
    With prior code, many "Tile" objects would be created and added to the "trackTracker" table when holding down the mouse, and "Tile" objects could be created in grid squares already displaying tracks. The "track_tracker" table of tables corresponds to the columns and rows, and it checks whether a "Tile" object exists for the corresponding position. Bug fixed, and overall memory and efficiency is improved.

--- main.lua
+++ main.lua
@@ -9,6 +9,13 @@ function love.load()
     mousex = 0
     mousey = 0
     trainTracks = {}
+    track_tracker = {}
+    for i = 0,15 do
+        table.insert(track_tracker, {})
+        for j = 0,8 do
+            table.insert(track_tracker[i + 1], nil)
+        end
+    end
 end
 
 function love.update(dt)
@@ -16,14 +23,18 @@ function love.update(dt)
     mouse_floor_x = math.floor(mousex / 64)
     mouse_floor_y = math.floor(mousey / 64)
     if love.mouse.isDown(1) then
-        local track = Tile(mouse_floor_x * x, mouse_floor_y * y)
-        table.insert(trainTracks, track)
+        if not track_tracker[mouse_floor_x + 1][mouse_floor_y + 1] then
+            local track = Tile(mouse_floor_x * x, mouse_floor_y * y)
+            table.insert(trainTracks, track)
+            track_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = track
+            print("track added")
+        end
     end
 end
 
 function love.draw()
-    for i=0,15 do
-        for j=0,8 do
+    for i = 0,15 do
+        for j = 0,8 do
             love.graphics.rectangle("line", x * i, y * j, gridw, gridh)
         end
     end
