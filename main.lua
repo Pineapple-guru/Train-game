6ac16dbabc2a1c09cf98aeef1bc3e05e03aa6518
James Ferguson <jamessferguson@student.cofo.edu>
2022-11-04 21:59:41 -0500

    Update main.lua; mouse_grid
    Updated main.lua such that it displays a 16x9 grid and fills in the square containing the mouse. The variables correlated to the grid of the mouse will be useful later on for adding tracks.

--- main.lua
+++ main.lua
@@ -1,17 +1,24 @@
 function love.load()
-  x = 64
-  y = 64
-  gridw = 64
-  gridh = 64
+    x = 64
+    y = 64
+    gridw = 64
+    gridh = 64
+    mousex = 0
+    mousey = 0
 end
 
 function love.update(dt)
+    mousex, mousey = love.mouse.getPosition()
+    print(math.floor(mousex / 64) + 1, math.floor(mousey / 64) + 1)
+    mouse_floor_x = math.floor(mousex / 64)
+    mouse_floor_y = math.floor(mousey / 64)
 end
 
 function love.draw()
-  for i=0,15 do
-    for j=0,8 do
-      love.graphics.rectangle('line', x * i, y * j, gridw, gridh)
+    for i=0,15 do
+        for j=0,8 do
+            love.graphics.rectangle("line", x * i, y * j, gridw, gridh)
+        end
     end
-  end
+    love.graphics.rectangle("fill", x * mouse_floor_x, y * mouse_floor_y, gridw, gridh)
 end
