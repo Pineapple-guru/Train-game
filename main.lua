f430fedcecb4db611ff5f2fd77f9138638966481
James Ferguson <jamessferguson@student.cofo.edu>
2022-11-05 11:04:52 -0500

    Update main.lua; track stack & deleting tracks
    Tracks can now be deleted with the right-click mouse button, and all tracks are stored in a stack so that previous locations can be referenced when track is deleted. Also, I believe in this update the love.draw() function was updated such that it draws all tiles

--- main.lua
+++ main.lua
@@ -15,15 +15,16 @@ function love.load()
     for i = 0,15 do
         table.insert(object_tracker, {})
         for j = 0,8 do
-            local empty_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "empty", "empty_tile.png")
+            local empty_tile = Tile(i * x, j * y, "empty", "empty_tile.png")
             table.insert(object_tracker[i + 1], empty_tile)
         end
     end
-    local start_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "start", "start_tile.png")
+    local start_tile = Tile(2 * x, 5 * y, "start", "start_tile.png")
     object_tracker[3][6] = start_tile
-    local end_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "end", "end_tile.png")
+    local end_tile = Tile(12 * x, 1 * y, "end", "end_tile.png")
     object_tracker[13][2] = end_tile
-    current_tile = {2, 5}
+    current_tile_stack = {}
+    table.insert(current_tile_stack, {2, 5})
 end
 
 function love.update(dt)
@@ -31,12 +32,18 @@ function love.update(dt)
     mouse_floor_x = math.floor(mousex / 64)
     mouse_floor_y = math.floor(mousey / 64)
     if love.mouse.isDown(1) then
-        --if object_tracker[mouse_floor_x + 1][mouse_floor_y + 1].name == "empty" then
-        if ((mouse_floor_x == current_tile[1] - 1 and mouse_floor_y == current_tile[2]) or (mouse_floor_x == current_tile[1] + 1 and mouse_floor_y == current_tile[2]) or (mouse_floor_x == current_tile[1] and mouse_floor_y == current_tile[2] - 1) or (mouse_floor_x == current_tile[1] and mouse_floor_y == current_tile[2] + 1)) and check_square(mouse_floor_x, mouse_floor_y) then
+        if ((mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] - 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] + 1 and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2]) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] - 1) or (mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] + 1)) and check_square(mouse_floor_x, mouse_floor_y) then
             local track = Tile(mouse_floor_x * x, mouse_floor_y * y, "track", "track_tile.png")
             table.insert(trainTracks, track)
             object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = track
-            current_tile = {mouse_floor_x, mouse_floor_y}
+            table.insert(current_tile_stack, {mouse_floor_x, mouse_floor_y})
+        end
+    end
+    if love.mouse.isDown(2) then
+        if mouse_floor_x == current_tile_stack[table.maxn(current_tile_stack)][1] and mouse_floor_y == current_tile_stack[table.maxn(current_tile_stack)][2] and object_tracker[mouse_floor_x + 1][mouse_floor_y + 1].name == "track" then
+            local empty_tile = Tile(mouse_floor_x * x, mouse_floor_y * y, "empty", "empty_tile.png")
+            object_tracker[mouse_floor_x + 1][mouse_floor_y + 1] = empty_tile
+            table.remove(current_tile_stack, table.maxn(current_tile_stack))
         end
     end
 end
@@ -47,32 +54,15 @@ function love.draw()
             love.graphics.rectangle("line", x * i, y * j, gridw, gridh)
         end
     end
-    love.graphics.print("start", x * 2, y * 5)
-    love.graphics.print("end", x * 12, y * 1)
-    love.graphics.rectangle("fill", x * mouse_floor_x, y * mouse_floor_y, gridw, gridh)
-    for i,track in ipairs(trainTracks) do
-        track:draw()
+    for i, column in ipairs(object_tracker) do
+        for j, tile in ipairs(column) do
+            tile:draw()
+        end
     end
 end
 
 function check_square(x, y)
     if x >= 0 and x <= 15 and y >= 0 and y <= 8 then
         return object_tracker[x + 1][y + 1].name == "empty"
-    end
-end
-    
-function available_tiles_finder(x, y)
-    option_table = {}
-    if check_square(x + 1, y) then
-        table.insert(option_table, {x + 1, y})
-    end
-    if check_square(x - 1, y) then
-        table.insert(option_table, {x - 1, y})
-    end
-    if check_square(x, y + 1) then
-        table.insert(option_table, {x, y + 1})
-    end
-    if check_square(x, y - 1) then
-        table.insert(option_table, {x, y - 1})
     end
 end
