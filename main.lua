6772e8ce385b4429b8398589c607bb628ce76045
mwasanje <thepineguru@gmail.com>
2022-11-04 21:34:10 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
+function love.load()
+  x = 64
+  y = 64
+  gridw = 64
+  gridh = 64
+end
 
+function love.update(dt)
+end
+
+function love.draw()
+  for i=0,15 do
+    for j=0,8 do
+      love.graphics.rectangle('line', x * i, y * j, gridw, gridh)
+    end
+  end
+end
