68114cf714505f2f08771b0834c8acc43a520a05
James Ferguson <jamessferguson@student.cofo.edu>
2022-11-05 22:51:53 -0500

    Update train.lua; changes icons based on direction
    Icons change based on the movement of the train object

--- train.lua
+++ train.lua
@@ -12,7 +12,11 @@ function Train:new(line_stack)
     for i, track in ipairs(self.line_stack) do
         table.insert(self.reverse_line_stack, 1, track)
     end
-    self.image = love.graphics.newImage("train_icon.png")
+    self.train_up = love.graphics.newImage("TrainUp.png")
+    self.train_down = love.graphics.newImage("TrainDown.png")
+    self.train_left = love.graphics.newImage("TrainLeft.png")
+    self.train_right = love.graphics.newImage("TrainRight.png")
+    self.image = love.graphics.newImage("TrainUp.png")
 end
 
 function Train:update(dt)
@@ -26,19 +30,23 @@ function Train:update(dt)
         if self.current_stack[self.current_index][1] == self.current_stack[self.current_index + 1][1] + 1 then
             self.x_ratio = self.x_ratio - (distance / 64)
             self.x = self.x - distance
+            self.image = self.train_left
         end
         if self.current_stack[self.current_index][1] == self.current_stack[self.current_index + 1][1] - 1 then
             self.x_ratio = self.x_ratio + (distance / 64)
             self.x = self.x + distance
+            self.image = self.train_right
         end
     else
         if self.current_stack[self.current_index][2] == self.current_stack[self.current_index + 1][2] + 1 then
             self.y_ratio = self.y_ratio - (distance / 64)
             self.y = self.y - distance
+            self.image = self.train_up
         end
         if self.current_stack[self.current_index][2] == self.current_stack[self.current_index + 1][2] - 1 then
             self.y_ratio = self.y_ratio + (distance / 64)
             self.y = self.y + distance
+            self.image = self.train_down
         end
     end
     if self.x_ratio >= 1 then
